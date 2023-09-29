103315f0a0eb7ed356108996faf538a18576b628
jakelord1 <jakelord1.vo@gmail.com>
2023-09-29 15:04:55 +0300

    Добавьте файлы проекта.

---
+++ HW_w4d2/HW_w4d2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <ctime>
+#include <windows.h>
+#include <queue>
+#include "Queue.h"
+using namespace std;
+
+int main()
+{
+    QueuePriority printer(10);
+    printer.Add("student", 1);
+    printer.Add("director", 5);
+    printer.Add("manager1", 4);
+    printer.Add("manager2", 3);
+    printer.Add("worker", 2);
+    printer.Show();
+
+    int c = 0;
+    time_t times;
+    char str[26] = {};
+
+    queue<string> statistic;
+    while (c != printer.GetCount()) {
+        statistic.push(printer.Extract());
+        Sleep(3000);
+        times = time(nullptr);
+        ctime_s(str, sizeof str, &times);
+        cout << statistic.front() << ' ' << str << endl;
+        statistic.pop();
+    }
+    
+    
+  
+    
+
+    
+
+    
+}
